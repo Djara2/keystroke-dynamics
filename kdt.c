6c4e8fce41315b45fae3a94696d8fc7393f7628c
Dave <jaradavid@protonmail.com>
2024-12-21 01:34:08 -0600

    Added sessions so multiple tests may be taken
    This allows for an overall testing session, e.g. where someone takes 10 (or whatever other number) 30 second (or whatever other number) typing tests. The data for all these tests is collected and saved.

--- kdt.c
+++ kdt.c
@@ -21,29 +21,46 @@ struct keystroke {
 };
 
 // Shared data structure for threads
-typedef struct {
+struct timer_state {
 	bool flag;
 	int seconds;
-	pthread_mutex_t lock;
-} SharedData;
+	// pthread_mutex_t lock;
+};
 
 struct phoneme {
 	char length;
 	char *str;
 };
 
+struct time_delta_array {
+	unsigned long *values;
+	size_t length;
+	size_t capacity;
+}
+;
 struct kv_pair {
 	struct phoneme *key;
-	unsigned long  *value;
+	struct time_delta_array *value;
 	struct kv_pair *next;
+};
+
+struct session {
+	struct keystroke *keystrokes;
+	size_t keystrokes_length;
+	struct time_delta_array *time_deltas;
 };
 
 struct phoneme* phoneme_create(char *str, byte length);
 bool phoneme_compare(struct phoneme *p1, struct phoneme *p2);
 
-struct kv_pair *kv_pair_create(struct phoneme *key, unsigned long *value);
+struct time_delta_array* time_delta_array_create(unsigned long *values, size_t length, size_t capacity);
 
-unsigned short hash_phoneme_struct(struct phoneme p);	
+struct kv_pair *kv_pair_create(struct phoneme *key, struct time_delta_array *value);
+
+short hash_phoneme_struct(struct phoneme *p);
+
+bool hashmap_set(struct kv_pair **hashmap, struct phoneme *key, struct time_delta_array *value);
+struct time_delta_array* hashmap_get(struct kv_pair **hashmap, struct phoneme *key);
 
 //                       0     1      2      3                4                5
 enum required_arguments {USER, EMAIL, MAJOR, TYPING_DURATION, NUMBER_OF_TESTS, OUTPUT_FILE_PATH};
@@ -54,7 +71,7 @@ void enable_buffering_and_echoing();
 
 void timer_function(void* arg);
 
-unsigned long* get_time_deltas_in_milliseconds(struct keystroke keystrokes[], size_t keystrokes_length);
+unsigned long* get_time_deltas_in_milliseconds(struct keystroke *keystrokes, size_t keystrokes_length);
 
 void display_help_text();
 void display_environment_details(char user[], char email[], char major[], int duration, short number_of_samples);
@@ -104,89 +121,145 @@ int main(int argc, char **argv) {
 	display_environment_details(user, email, major, typing_duration, number_of_tests);
 
 	// Initialize shared data for timer fucntion
-	SharedData shared_data = {
+	struct timer_state timer = {
 		.flag = true,
-		.seconds = atoi(argv[2])
+		.seconds = typing_duration
 	};
-	pthread_mutex_init(&shared_data.lock, NULL);
 	
 	size_t keystrokes_length = 0;
 	size_t keystrokes_capacity = 2048;
-	struct keystroke keystrokes[keystrokes_capacity];
+	struct keystroke *keystrokes = malloc(sizeof(struct keystroke) * keystrokes_capacity);
+	if(keystrokes == NULL) {
+		fprintf(stderr, "Failed to allocate memory for initial 2048 keystrokes.\n");
+		exit(EXIT_FAILURE);
+	}
 
 	unsigned char c;
 	unsigned char bytes_read = 0;
 
-	// Prompt
-	printf("rawInputTool$ "); 
-	fflush(stdout);
+	struct session sessions[number_of_tests];
+	byte sessions_length = 0;
+	unsigned long *time_deltas;
 
-	// Enter non-canonical mode without echoing to collect raw data
-	disable_buffering_and_echoing();
-
-	// Create thread for timer function
 	pthread_t timer_thread;
-	if(pthread_create(&timer_thread, NULL, (void*)timer_function, &shared_data) != 0) {
-		fprintf(stderr, "Error creating timer thread.\n");
-		return 1;
-	}
+	for(int session_number = 0; session_number < number_of_tests; session_number++) {
+		// Prompt
+		printf("rawInputTool$ "); 
+		fflush(stdout);
 
-	// Actually collect the raw data
-	while(shared_data.flag == true) {
-		bytes_read = read(STDIN_FILENO, &c, 1);
-		if(bytes_read <= 0) continue;
+		// Enter non-canonical mode without echoing to collect raw data
+		disable_buffering_and_echoing();
+
+		// Set timer appropriately
+		timer.flag = true;
+
+		// Create thread for timer function
+		if(pthread_create(&timer_thread, NULL, (void*)timer_function, &timer) != 0) {
+			fprintf(stderr, "Error creating timer thread.\n");
+			return 1;
+		}
+
+		// Actually collect the raw data
+		while(timer.flag == true) {
+			bytes_read = read(STDIN_FILENO, &c, 1);
+			if(bytes_read <= 0) continue;
+			
+			// Capture character stroke and timestamp
+			if(keystrokes_length < keystrokes_capacity) {
+				keystrokes[keystrokes_length].c = c;
+				clock_gettime( CLOCK_MONOTONIC, &(keystrokes[keystrokes_length].timestamp) );
+				keystrokes_length++;
+			}
+
+			// echo back what was written
+			switch(c) {
+				case BACKSPACE:
+					printf("\b \b");
+					fflush(stdout);
+					break;
+
+				default:
+					printf("%c", c);
+					fflush(stdout);
+					break;
+			}
+		}
 		
-		// Capture character stroke and timestamp
-		if(keystrokes_length < keystrokes_capacity) {
-			keystrokes[keystrokes_length].c = c;
-			clock_gettime( CLOCK_MONOTONIC, &(keystrokes[keystrokes_length].timestamp) );
-			keystrokes_length++;
+		// Restore canonical mode and echoing
+		fflush(stdout);
+		enable_buffering_and_echoing();
+
+		// Print the numeric values of keys pressed for current session
+		printf("\nNumeric codes entered:\n");
+		printf("\n%d", (int) keystrokes[0].c);
+		for(size_t i = 1; i < keystrokes_length ; i++) 
+			printf(", %d", (int) keystrokes[i].c);
+
+		printf("\n");
+
+		// Get time deltas for current session
+		time_deltas = get_time_deltas_in_milliseconds(keystrokes, keystrokes_length);
+		if(time_deltas == NULL) {
+			fprintf(stderr, "Could not get time deltas.\n"); 
 		}
+		
+		// Print the time deltas for the current session
+		printf("\nTime deltas:\n");
+		printf("%ld", time_deltas[0]);
+		for(size_t i = 1; i < keystrokes_length - 1; i++) 
+			printf(", %ld", time_deltas[i]);
 
-		// echo back what was written
-		switch(c) {
-			case BACKSPACE:
-				printf("\b \b");
-				fflush(stdout);
-				break;
+		printf("\n");
+		
+		// Join thread
+		printf("[DEBUG] Attempting to join timer pthread...\n");
+		pthread_join(timer_thread, NULL);
+		printf("[DEBUG] Timer pthread joined!\n");
 
-			default:
-				printf("%c", c);
-				fflush(stdout);
-				break;
+		// Move data into nearest, unused session struct
+		printf("[DEBUG] Attempting to allocate memory for session %d's keystrokes buffer...\n", session_number + 1);
+		sessions[session_number].keystrokes = malloc(sizeof(struct keystroke) * keystrokes_length);
+		if(sessions[session_number].keystrokes == NULL) {
+			fprintf(stderr, "Failed to allocate memory for session %d's keystrokes buffer.\n", session_number + 1);
+
+			// Free other sessions to prevent memory leak
+			for(int i = 0; i < session_number; i++) {
+				if(sessions[session_number].keystrokes != NULL) 
+					free(sessions[session_number].keystrokes);
+				
+				if(sessions[session_number].time_deltas != NULL) {
+					free(sessions[session_number].time_deltas->values);
+					free(sessions[session_number].time_deltas);
+				}
+			}	
 		}
+		printf("[DEBUG] Session %d's keystrokes buffer was allocated successfully!\n", session_number + 1);
+		printf("[DEBUG] Attempting to copy %d keystrokes into session %d's keystrokes buffer...\n", keystrokes_length, session_number + 1);
+		memcpy(sessions[session_number].keystrokes, keystrokes, sizeof(struct keystroke) * keystrokes_length);
+		printf("[DEBUG] Memory copied!\n");
+		sessions[session_number].keystrokes_length = keystrokes_length;
+		
+		// Copy over time deltas (there are always N-1 time deltas where N is the number of keystrokes) 
+		if(time_deltas != NULL) {
+			printf("[DEBUG] Attempting to construct time_delta_array for session %d and have it point to the time deltas found with get_time_deltas_in_milliseconds...\n", session_number + 1);
+			sessions[session_number].time_deltas = time_delta_array_create(time_deltas, keystrokes_length - 1, keystrokes_length - 1);
+			printf("[DEBUG] Function call succeeded!\n");
+			if(sessions[session_number].time_deltas == NULL)
+				fprintf(stderr, "Failed to create new time delta array for session number %d.\n", session_number + 1);
+		}	
+		else
+			sessions[session_number].time_deltas = NULL;
 
+		// Reset buffer for next run. There is no need to clear out the keystrokes buffer, because
+		// the session struct has its keystrokes member set using memcpy.
+		keystrokes_length = 0;
+
+		// Prompt user before continuing to next test
+		printf("\n[SYSTEM] Press ENTER to take next test... ");
+		c = 1;
+		while(c != '\n') 
+			c = fgetc(stdin);
 	}
-	
-	// Restore canonical mode and echoing
-	fflush(stdout);
-	enable_buffering_and_echoing();
-
-	// Print the numeric values of keys pressed
-	printf("\nNumeric codes entered:\n");
-	printf("\n%d", (int) keystrokes[0].c);
-	for(size_t i = 1; i < keystrokes_length ; i++) 
-		printf(", %d", (int) keystrokes[i].c);
-
-	printf("\n");
-	unsigned long *time_deltas = get_time_deltas_in_milliseconds(keystrokes, keystrokes_length);
-	if(time_deltas == NULL) {
-		fprintf(stderr, "Could not get time deltas.\n"); 
-	}
-
-	printf("\nTime deltas:\n");
-	printf("%ld", time_deltas[0]);
-	for(size_t i = 1; i < keystrokes_length - 1; i++) 
-		printf(", %ld", time_deltas[i]);
-
-	printf("\n");
-
-
-
-	pthread_join(timer_thread, NULL);
-
-	// Clean up thread mutex
-	pthread_mutex_destroy(&shared_data.lock);
 
 	printf("Program terminated.\n");
 	return 0;
@@ -209,17 +282,15 @@ void enable_buffering_and_echoing() {
 }
 
 void timer_function(void* arg) {
-	SharedData* data = (SharedData*)arg;
+	struct timer_state *data = (struct timer_state*) arg;
 
 	sleep(data->seconds);
 
-	pthread_mutex_lock(&data->lock);
+	// pthread_mutex_lock(&data->lock);
 	data->flag = false;
-	printf("\nTimer has elapsed! Flag changed to: %s\n", data->flag ? "true" : "false");
-	pthread_mutex_unlock(&data->lock);
 }
 
-unsigned long* get_time_deltas_in_milliseconds(struct keystroke keystrokes[], size_t keystrokes_length) {
+unsigned long* get_time_deltas_in_milliseconds(struct keystroke *keystrokes, size_t keystrokes_length) {
 	if(keystrokes == NULL) return NULL;
 		
 	unsigned long *time_deltas = malloc(sizeof(unsigned long) * (keystrokes_length - 1));
@@ -266,7 +337,7 @@ void display_help_text() {
 }
 
 void display_environment_details(char user[], char email[], char major[], int duration, short number_of_samples) {
-	printf("Environment:\n\tUser: %s\n\tEmail: %s\n\tMajor: %s\n\tTyping duration: %d\n\tSamples to take: %hd\n",
+	printf("Environment:\n\tUser: %s\n\tEmail: %s\n\tMajor: %s\n\tTyping duration: %d\n\tSamples to take: %hd\n\n",
 			user, email, major, duration, number_of_samples);
 }
 
@@ -460,8 +531,22 @@ bool phoneme_compare(struct phoneme *p1, struct phoneme *p2) {
 	return true;
 }
 
+struct time_delta_array* time_delta_array_create(unsigned long *values, size_t length, size_t capacity) {
+	struct time_delta_array *new_time_delta_array = malloc(sizeof(struct time_delta_array));
+	if(new_time_delta_array == NULL) {
+		fprintf(stderr, "[time_delta_array_create] Failed to allocate memory for a new time delta array.\n");
+		return NULL;
+	}
+
+	new_time_delta_array->values = values;
+	new_time_delta_array->length = length;
+	new_time_delta_array->capacity = capacity;
+
+	return new_time_delta_array;
+}
+
 // key is required, but time deltas is not (it can be NULL). Just check for this later
-struct kv_pair *kv_pair_create(struct phoneme *key, unsigned long *value) {
+struct kv_pair *kv_pair_create(struct phoneme *key, struct time_delta_array *value) {
 	if(key == NULL) {
 		fprintf(stderr, "[kv_pair_create] Cannot create a new key-value pair with a phoneme key that points to NULL.\n");
 		return NULL;
@@ -476,11 +561,11 @@ struct kv_pair *kv_pair_create(struct phoneme *key, unsigned long *value) {
 
 	// Assign key and value
 	new_kv_pair->key = key;	
-	new_kv_pair->value = value; // might be NULL if creating key for first time.
-	new_kv_pair->next = NULL 
+	new_kv_pair->value = value;
+	new_kv_pair->next = NULL;
 }
 
-unsigned short hash_phoneme_struct(struct phoneme *p) {
+short hash_phoneme_struct(struct phoneme *p) {
 	if(p == NULL) {
 		fprintf(stderr, "[hash_phoneme_struct] Cannot hash phoneme struct that points to NULL.\n");
 		return -1;
@@ -493,4 +578,96 @@ unsigned short hash_phoneme_struct(struct phoneme *p) {
 	hash *= HASH_SCALAR;
 	hash = hash % MODULUS;
 	return (unsigned short) hash;
+}
+
+bool hashmap_set(struct kv_pair **hashmap, struct phoneme *key, struct time_delta_array *value) {
+	if(hashmap == NULL) {
+		fprintf(stderr, "[hashmap_insert] Insertion into a hashmap that points to NULL cannot be done.\n");
+		return false;
+	}
+
+	if(key == NULL) {
+		fprintf(stderr, "[hashmap_insert] Insertion of a phoneme key that points to NULL cannot be done.\n");
+		return false;
+	}
+
+	short hashed_index = hash_phoneme_struct(key);
+	if(hashed_index == -1) { 
+		fprintf(stderr, "[hashmap_insert] Hashed index came back as -1 for provided phoneme key.\n");
+		return false;
+	}
+	
+	// Case 1: Index is NULL, meaning it is has never been used before and is free for insertion
+	if(hashmap[hashed_index] == NULL) {
+		hashmap[hashed_index] = kv_pair_create(key, value);
+		if(hashmap[hashed_index] == NULL) {
+			fprintf(stderr, "[hashmap_insert] Index %hd assigned to NULL after call to kv_pair_create.\n");
+			return false;
+		}
+		return true;
+	}
+	else {
+	// Case 2: Index is occupied. The key may or may not already exist
+		struct kv_pair *current = hashmap[hashed_index];
+		while(current->next != NULL)
+		{
+			// Case 2.1: Key already exists, just as a node in a linear chain. Update value and then return true.
+			if(phoneme_compare(hashmap[hashed_index]->key, key) == 0) {
+				// Prevent memory leaks
+				if(hashmap[hashed_index]->value != NULL) {
+					free(hashmap[hashed_index]->value->values);
+					free(hashmap[hashed_index]->value);
+				}
+				// Update value mapped by pre-existing key and return
+				hashmap[hashed_index]->value = value;
+				return true;
+			}
+		
+			current = current->next;
+		}
+		// Case 2.2: Key does NOT exist. Add it via linear probing.
+		current->next = kv_pair_create(key, value);
+		if(current->next == NULL) {
+			fprintf(stderr, "[hashmap_set] Failed to insert new hashmap key. Call to kv_pair_create returned NULL.\n");
+			return false;
+		}
+		return true;
+	}
+}
+
+struct time_delta_array* hashmap_get(struct kv_pair **hashmap, struct phoneme *key) {
+	if(hashmap == NULL) {
+		fprintf(stderr, "[hashmap_get] Cannot retrieve values from hashmap that points to NULL.\n");
+		return NULL;
+	}
+	if(key == NULL) {
+		fprintf(stderr, "[hashmap_get] Cannot search hashmap for a key that points to NULL.\n");
+		return NULL;
+	}
+
+	short hashed_index = hash_phoneme_struct(key);
+	if(hashed_index == -1) {
+		fprintf(stderr, "[hashmap_get] Failed to hash provided key. Function hash_phoneme_struct returned -1.\n");
+		return NULL;
+	}
+	
+	// Case 1: Immediate miss
+	if(hashmap[hashed_index] == NULL) {
+		fprintf(stderr, "[hashmap_get] Cannot get the value mapped to a key that does not exist in the hashmap.\n");
+		return NULL;
+	}
+	else{
+		// Do linear probing
+		struct kv_pair *current = hashmap[hashed_index];
+		while(current->next != NULL) {
+			// Case 2.1: Key found, return value
+			if(phoneme_compare(current->key, key) == 0)
+				return current->value;
+			
+			current = current->next;
+		}
+
+		// Case 2.2: Key does not exist in the hashmap, return NULL
+		return NULL;
+	}
 }
