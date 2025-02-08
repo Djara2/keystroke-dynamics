0473d97c3bc6d5ea79a44b9fa43f9d9daa17bb5e
LaTechBen <bjm056@email.latech.edu>
2025-02-07 21:53:00 -0600

    Kolmogorov-Smirnov test update and Feature Selection
    Updeated the kolmogorov_smirnov_test function to the latest standard. Included pearson_correlation for feature selection that returns a mapping of the feature to its importance.

--- algorithms.py
+++ algorithms.py
@@ -1,52 +1,113 @@
-import numpy as np
-from collections import Counter
-import random
-from scipy.stats import norm, ks_2samp
-
-percent_to_train = 0.8
-alpha = 0.05
+# import numpy as np
+# from collections import Counter
+# import random
+# from scipy.stats import norm, ks_2samp
+from enum import auto, Enum
 
 
-def knn_euclidean(training_data, classifier_list, test_point, k):
-    distances = []
-    for i in range(len(training_data)):
-        distance = np.sqrt( np.sum( (np.array(test_point) - np.array(training_data[i]))**2 ) )
-        distances.append((distance, classifier_list[i]))
-    distances.sort(key=lambda x: x[0])
-    k_nearest_labels = [label for _, label in distances[:k]]
-    return Counter(k_nearest_labels).most_common(1)[0][0]
+class Error(Enum):
+    SUCCESS = auto()
+    INVALID_USER = auto()
+    LIBRARY_FAILURE = auto()
+    NO_DATA = auto()
+
+
+class Constant(Enum):
+    ALPHA = 0.05
+    TRAINING_PERCENT = 0.8
+    CORRELATION_COEFFICIENT = 0.6
+
+
+# Feature Selection
+def pearson_correlation(raw_data: dict) -> dict:
+    # Check to make sure data exists
+    if raw_data == None:
+        return Error.NO_DATA
+    feature_list: list = [key for key in raw_data.keys()] # converting to list for iterability
+    # Assign an importance of 1 to each feature. The idea is to compare two features for correlation and if they are correlated, reduce the importance of one feature to 0.
+    feature_importance: dict = {key:1 for key in feature_list}
+    n: int = len(raw_data[0]) # n needs to be the number of rows in the data
+    for index_left in range(n-1):
+        left_feature: str = feature_list[index_left]
+        for index_right in range(index_left, n-1):
+            right_feature: str = feature_list[index_right]
+            if(feature_importance[left_feature] and feature_importance[right_feature]):
+                #  The link to the formula used: https://www.geeksforgeeks.org/pearson-correlation-coefficient/
+                x_data: list = feature_list[index_left]
+                y_data: list = feature_list[index_right]
+                xy_data: list = [x+y for x in x_data for y in y_data]
+                x_squared: list = [x*x for x in x_data]
+                y_squared: list = [y*y for y in y_data]
+
+                x_sum: int = sum(x_data)
+                y_sum: int = sum(y_data)
+                xy_sum: int = sum(xy_data)
+                x_squared_sum: int = sum(x_squared)
+                y_squared_sum: int = sum(y_squared)
+
+                r_numerator = ( n * xy_sum ) - ( x_sum * y_sum )
+                r_denomenator_left = ( n * x_squared_sum ) - ( x_sum ** 2 )
+                r_denomenator_right = ( n * y_squared_sum ) - ( y_sum ** 2 )
+
+                r_value = r_numerator / ( ( r_denomenator_left * r_denomenator_right ) ** .5 )
+
+                if(r_value < Constant.CORRELATION_COEFFICIENT):
+                    feature_importance[right_feature] = 0
+    # returning the feature importance dictionary. This will be used to know which features are important if they have a 1. 
+    return feature_importance
 
 
 
 
-def knn_manhattan(training_data, classifier_list, test_point, k):
-    distances = []
-    for i in range(len(training_data)):
-        distance = ( np.sum( np.array(test_point) - np.array(training_data[i]) ) )
-        distances.append((distance, classifier_list[i]))
-    distances.sort(key=lambda x: x[0])
-    k_nearest_labels = [label for _, label in distances[:k]]
-    return Counter(k_nearest_labels).most_common(1)[0][0]
 
-
-# Kolmogorov-Smirno Test
-train_data = []
-def kolmogorov_smirnov_classifier(data_row):
-    train_data.append(data_row)
+# # Classifiers
+# def knn_euclidean(training_data, classifier_list, test_point, k):
+#     distances = []
+#     for i in range(len(training_data)):
+#         distance = np.sqrt( np.sum( (np.array(test_point) - np.array(training_data[i]))**2 ) )
+#         distances.append((distance, classifier_list[i]))
+#     distances.sort(key=lambda x: x[0])
+#     k_nearest_labels = [label for _, label in distances[:k]]
+#     return Counter(k_nearest_labels).most_common(1)[0][0]
 
 
 
-def kolmogorov_smirnov_test(test_data, user):
+
+# def knn_manhattan(training_data, classifier_list, test_point, k):
+#     distances = []
+#     for i in range(len(training_data)):
+#         distance = ( np.sum( np.array(test_point) - np.array(training_data[i]) ) )
+#         distances.append((distance, classifier_list[i]))
+#     distances.sort(key=lambda x: x[0])
+#     k_nearest_labels = [label for _, label in distances[:k]]
+#     return Counter(k_nearest_labels).most_common(1)[0][0]
+
+
+# # Kolmogorov-Smirno Test
+# train_data: list = []
+# def kolmogorov_smirnov_classifier(data_row: dict) -> None:
+#     train_data.append(data_row)
+
+
+
+def kolmogorov_smirnov_test(train_data: list[dict], test_data: dict) -> bool|Error:
+    # returns a boolean except on error, which then returns None
     try:
-        # Perform the ks test on the digraph and trigraph independent of each other. 
+        user: str = test_data["user"]
+        user_data: list[dict] = [user_session for user_session in train_data if user_session["user"] == user]
+    except:
+        print(f'[kolmogorov_smirnov_test] The user provided was not found within the training data.\n')
+        return Error.INVALID_USER
+    try:
+        # Perform the kolmogorov-smirnov test on the digraph and trigraph independent of each other. 
         di_ks_statistic, di_p_value = ks_2samp(test_data[user]["digraph"], train_data[user]["digraph"])
         tri_ks_statistic, tri_p_value = ks_2samp(test_data[user]["trigraph"], train_data[user]["trigraph"])
 
-        # False represents a rejection of the null hypothesis whereas a True represents a failure to reject the null hypothesis which in a way means an acceptance.
-        return False if di_p_value < alpha and tri_p_value < alpha else True
-    except:
-        print("[General Error] User may not be in the dataset OR incorrect feature labels OR ks_2samp errored.")
-        return
+        # False represents a rejection of the null hypothesis whereas a True represents a failure to reject the null hypothesis, which in a way means an acceptance.
+        return False if di_p_value < Constant.ALPHA and tri_p_value < Constant.ALPHA else True
+    except Exception as e:
+        print(f'[kolmogorov_smirnov_test] An error occured when running the ks_2samp function from the scipy.stats library.\nError from library:\n{e}\n')
+        return Error.LIBRARY_FAILURE
 
 
 
